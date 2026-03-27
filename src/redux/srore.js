3b8c54baaef84e63bc2df3b131bf950fb6506e74
NecoKab2011 <142300074+NecoKab2011@users.noreply.github.com>
2026-03-27 21:40:56 +0100

    Updates

--- src/redux/srore.js
+++
@@ -1,9 +0,0 @@
-import { configureStore, combineReducers } from "@reduxjs/toolkit";
-import { tasksReducer, filtersReducer } from "./reducer";
-
-export const store = configureStore({
- reducer: {
-   tasks: tasksReducer,
-   filters: filtersReducer,
- },
-});
